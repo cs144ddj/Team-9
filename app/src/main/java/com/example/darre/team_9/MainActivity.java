8df9b19952c7366ae106ae8ca68e25a120db62c3
Darren Jayasinghe <Darren Jayasinghe>
2016-12-26 14:23:03 +0000

    Created a blank application for the search engine

--- app/src/main/java/com/example/darre/team_9/MainActivity.java
+++ app/src/main/java/com/example/darre/team_9/MainActivity.java
@@ -2,8 +2,16 @@ package com.example.darre.team_9;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView ListSearch;
+    private EditText editSearch;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
